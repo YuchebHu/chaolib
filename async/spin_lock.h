e055b77e5c5d0a4d63e821d3fcb0f62ffc2f8398
Hu Yuchen <Yuchebhu@outlook.com>
2025-03-12 20:37:47 +0800

    添加自旋锁实现及相关测试，更新 CMake 配置以包含新头文件

---
+++ async/spin_lock.h
@@ -0,0 +1,24 @@
+#ifndef __SPIN_LOCK_H__
+#define __SPIN_LOCK_H__
+#include <atomic>
+
+namespace chelib {
+namespace async {
+class SpinLock {
+public:
+  void lock() {
+    while (this->flag_.test_and_set(std::memory_order_acquire)) {
+      ;
+    }
+  }
+
+  void unlock() { this->flag_.clear(std::memory_order_release); }
+
+private:
+  std::atomic_flag flag_ = ATOMIC_FLAG_INIT;
+};
+} // namespace async
+using SpinLock = async::SpinLock;
+
+} // namespace chelib
+#endif // __SPIN_LOCK_H__
---
+++ tests/spin_lock.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <latch>
+#include <syncstream>
+#include <thread>
+
+#include "async/spin_lock.h"
+
+#define sync_stream std::osyncstream(std::cout)
+
+using namespace chelib;
+
+int main() {
+  SpinLock spin_lock;
+  spin_lock.lock();
+  std::latch latch(4);
+  for (auto i : {1, 2, 3, 4}) {
+    std::thread{[&spin_lock, &latch, i] {
+      spin_lock.lock();
+      sync_stream << "thread " << i << std::endl;
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      spin_lock.unlock();
+      latch.count_down();
+    }}.detach();
+  }
+  spin_lock.unlock();
+  latch.wait();
+
+  return 0;
+}
