af5a6c96cb6683d5a242e68451c0e2ceff2346ef
Hu Yuchen <Yuchebhu@outlook.com>
2025-04-03 21:43:55 +0800

    更新 README.md 以包含递归克隆和子模块更新的说明；调整 MemberProperty 结构体的成员变量格式

--- serializer/member_property.h
+++ serializer/member_property.h
@@ -3,17 +3,18 @@
 
 #pragma once
 
+#include <array>
 #include <string_view>
 
 namespace chaolib {
 namespace serializer {
 template <typename Class, typename T, int N> struct MemberProperty {
   constexpr MemberProperty(
-      T Class::*member,
+      T Class::* member,
       std::array<std::string_view, N> names)
       : member_(member), names_(std::move(names)) {}
 
-  T Class::*member_;
+  T Class::* member_;
   std::array<std::string_view, N> names_;
 };
 
